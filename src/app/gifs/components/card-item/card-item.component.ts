39c51b676ea76b66e1ab8fe60e09f142ea139968
Abel Borit <abelboritguitton@gmail.com>
2023-12-26 15:23:59 -0500

    [FEAT] search-box.component & card-item.component

---
+++ src/app/gifs/components/card-item/card-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.css'],
+})
+export class CardItemComponent {}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardItemComponent],
   exports: [HomePageComponent],
   imports: [CommonModule],
 })
